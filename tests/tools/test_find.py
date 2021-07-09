d9e603940dcf9e7a56a018adf7dd1aaf4124bfd4
switchupcb <contact@switchupcb.com>
2021-07-09 18:14:27 -0500

    srt tools: add find test cases

--- tests/tools/test_find.py
+++ tests/tools/test_find.py
@@ -39,19 +39,29 @@ class TestToolRemove(unittest.TestCase):
         result = find_by_timestamp([], t("00:00:30,000"), t("00:00:00,000"))
         self.assertEqual(list(result), [])
 
-        result = find_by_timestamp(self.subs(), self.x[0].start, self.x[0].start)
-        self.assertEqual(list(result), self.x)  # equivalent
-
         result = find_by_timestamp(self.subs(), t("00:00:19,738"), t("00:00:11,000"))
         self.assertEqual(list(result), [])
 
+        result = find_by_timestamp(self.subs(), t("00:00:11,000"), t("00:00:12,701"))
+        self.assertEqual(list(result), [self.x[0]])  # first
+
+        result = find_by_timestamp(self.subs(), t("00:00:16,538"), t("00:00:14,203"))
+        a = [
+            srt.Subtitle(1, t("00:00:11,000"), t("00:00:12,701"), "A"),
+            srt.Subtitle(2, t("00:00:12,701"), t("00:00:14,203"), "B"),
+            srt.Subtitle(3, t("00:00:16,538"), t("00:00:17,272"), "D"),
+            srt.Subtitle(4, t("00:00:16,538"), t("00:00:19,738"), "C"),
+            srt.Subtitle(5, t("00:00:17,272"), t("00:00:18,440"), "E"),
+        ]
+        self.assertEqual(list(result), a)  # middle
+
         result = find_by_timestamp(self.subs(), self.x[0].end, self.x[0].start)
         self.assertEqual(
-            list(result), sort([self.x[1], self.x[2], self.x[3], self.x[4]])
+            list(result), sort([self.x[1], self.x[2], self.x[3], self.x[4]])  # last
         )
 
         result = find_by_timestamp(self.subs(), t("00:00:14,500"), self.x[0].start)
-        self.assertEqual(list(result), sort([self.x[2], self.x[3], self.x[4]]))
+        self.assertEqual(list(result), sort([self.x[2], self.x[3], self.x[4]]))  # edges
 
         result = find_by_timestamp(self.subs(), t("00:00:17,500"), t("00:00:00,000"))
         a = [
@@ -68,7 +78,7 @@ class TestToolRemove(unittest.TestCase):
             srt.Subtitle(1, t("00:00:00,000"), t("00:00:01,701"), "A"),
             srt.Subtitle(2, t("00:00:01,701"), t("00:00:03,203"), "B"),
         ]
-        self.assertEqual(list(result), a)  # first subtitle
+        self.assertEqual(list(result), a)  # first - second
 
     def test_find_nonsequential_adjust(self):
         result = find_by_timestamp(
@@ -79,7 +89,7 @@ class TestToolRemove(unittest.TestCase):
             srt.Subtitle(2, t("00:00:02,038"), t("00:00:02,772"), "D"),
             srt.Subtitle(3, t("00:00:02,772"), t("00:00:03,940"), "E"),
         ]
-        self.assertEqual(list(result), a)
+        self.assertEqual(list(result), a)  # to first caption
 
         result = find_by_timestamp(
             self.subs(), t("00:00:17,500"), t("00:00:00,000"), True
